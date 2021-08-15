76ec771b0d7b59af51507f7378b1f28bfde11c28
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-15 13:25:56 +0300

    DZ (1/3) and Winners programm

---
+++ Lesson_3.py
@@ -0,0 +1,28 @@
+'''
+1: Даны два произвольные списка. Удалите из первого списка элементы присутствующие во втором списке.
+'''
+
+my_list_1 = [2, 5, 8, 2, 12, 12, 4]
+my_list_2 = [2, 5, 12, 4]
+
+my_list_1 = set(my_list_1)
+my_list_2 = set(my_list_2)
+
+print(my_list_1 - my_list_2)
+
+#winners
+print('СОРЕВНОВАНИЯ ПО PYTHON!')
+count = int(input('Введите количество участников: '))
+i = count
+members = []
+
+while i > 0:
+    name = input('Кто занял {} место: '.format(i))
+    members.append(name)
+    i -= 1
+
+print('В соревновании учавствовали: ', members)
+members.reverse()
+result = members[:3]
+result = 'Победители: {}. Поздравляем!'.format(result)
+print(result)
