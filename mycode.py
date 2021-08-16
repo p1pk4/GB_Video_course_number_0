26b129e1496934684b3c17922074701f315e048a
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-16 19:52:23 +0300

    описание и работа со списками v1

--- mycode.py
+++ mycode.py
@@ -9,15 +9,21 @@ third_digit = -1.3
 
 print(str(one_digit) + ' раз можно повторить: ' + proposal)
 
-#Список
-my_list = ['a', 'd', 'd', 'g', 'f']
+#Список. Элемент списка начинается с 0
+my_list = ['a', 'b', 'c', 'd', 'e', 1, 2, 3, 2.0, -14, True, 'word']
+print(my_list[7])
+#Индекс -1 выведет последний элемент из списка ('word')
+print(my_list[-1])
 
+'''
 #Цикл
 for word in proposal:
     print(word)
+'''
 
 count_my_list = len(my_list)
 print(count_my_list)
+
 #x = ''
 #my_list.count(x)
 #my_list.append(proposal)
