6bab9cbf7431dfbac35b3c5c44d0b66694398d35
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-16 19:43:49 +0300

    other

---
+++ mycode.py
@@ -0,0 +1,29 @@
+#import this
+#строка
+proposal = 'Для тех кто не в курсе, Пайтон самый легкий ЯП в освоении!!!11'
+
+#Числа
+one_digit = 100
+second_digit = 0.2
+third_digit = -1.3
+
+print(str(one_digit) + ' раз можно повторить: ' + proposal)
+
+#Список
+my_list = ['a', 'd', 'd', 'g', 'f']
+
+#Цикл
+for word in proposal:
+    print(word)
+
+count_my_list = len(my_list)
+print(count_my_list)
+#x = ''
+#my_list.count(x)
+#my_list.append(proposal)
+
+print(my_list)
+
+
+result = (one_digit ** (second_digit * third_digit))
+print(result)
