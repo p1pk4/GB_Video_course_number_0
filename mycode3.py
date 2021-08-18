a80b76ef88377914d9d576bf3e242802aed901a2
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-18 22:04:07 +0300

    add cycles and if elif

--- mycode3.py
+++ mycode3.py
@@ -1,13 +1,33 @@
-my_list1 = [1, 2, 3, 5, 6, 4]
-my_list2 = ['d', 'a', 'c', 'b']
+#ввести с клавы цифры или буквы, добавить ввод в список. Если введены буквы, сравнить с дефольным списком цифр
+#и наоборот и сравнить их в цикле
 
-my_list1.sort()
-my_list2.sort()
-print(my_list1, my_list2)
+#Пользовательский ввод списков 
+digitals = [int(i) for i in input('Введите цифры через пробел(не по порядку): ').split()]
+words = [str(f) for f in input('Введите буквы через пробел(не по порядку): ').split()]
 
-if len(my_list1) <= len(my_list2):
-    for list1 in my_list1:
-        print(list1)
-else:
-    for list2 in my_list2:
-        print(list2)
+#Сортировка списков
+digitals.sort()
+words.sort()
+
+#Прибавляет или отнимает еденицу от значения в индексах в списке цифр
+if len(digitals) < 10:
+    for digital in digitals:
+        #Пользователь вводит элементы списка пока список не станет равен 15, потом break
+        digitals.append(input()) #+= input(digitals.append) #+= 1
+        print(digitals)
+        if digitals == 15:
+            break
+#count запихнуть
+elif len(digitals) > 15:
+    for digital in digitals:
+        digital -= 1
+        print(digitals)
+
+#Ветвление списков
+if len(digitals) == len(words):
+    print('Список цифр {} и список букв {} равны'.format(digitals, words))
+elif len(digitals) > len(words):
+    print('Вы ввели цифры: {} и этот список длиннее списка букв'.format(digitals))
+elif len(digitals) < len(words):
+    print('Список букв {} длиннее чем список с цифрами'.format(words))
+
