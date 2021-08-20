0c40dd694528be3f194adb81f3760a985cd76a59
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-20 20:53:48 +0300

    dz

---
+++ Lesson4.py
@@ -0,0 +1,27 @@
+'''
+friends = 'Mike Donna'
+print(friends)
+
+#Поиск заданных символов в строке
+print(friends.find('Donna'))
+
+#Разделяет строки и делает из них список
+print(friends.split())
+
+#Проверяет состоит строчка из чисел или нет
+print(friends.isdigit())
+
+#Приводит все буквы к верхнему регистру
+print(friends.upper())
+
+#Приводит все буквы к нижнему регистру
+print(friends.lower())
+'''
+top5 = 'Первые 5 мест на соревнованиях: 1. Иванов 2. Петров 3. Сидоров 4. Зиньгельрман 5. Тревор'
+
+start = top5.find('1')
+end = top5.find('4')
+
+top3 = top5[start: end]
+
+result = print('Поздравляем {}с успехом!'.format(top3.upper()))
