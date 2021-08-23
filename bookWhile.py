af15af71c0366e2f3f863eae92d9f7133793692b
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-23 09:16:31 +0300

    book

---
+++ bookWhile.py
@@ -0,0 +1,21 @@
+number = 23
+running = True
+while running:
+    guess = int(input('Введите целое число : '))
+    if guess == number:
+        print('Поздравляю, вы угадали.')
+        running = False # это останавливает цикл while
+    elif guess < number:
+        print('Нет, загаданное число немного больше этого.')
+    else:
+        print('Нет, загаданное число немного меньше этого.')
+else:
+    print('Цикл while закончен.')
+# Здесь можете выполнить всё что вам ещё нужно
+print('Завершение.')    
+
+
+for i in list(range(1, 20, 3)):
+    print(i)
+else:
+    print('Цикл for закончен')
